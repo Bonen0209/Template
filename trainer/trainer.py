29c552f67551c1d5e456ca746732fc6ba4cd5d31
Wu Bo-Run's PC Ubuntu <r08942073@ntu.edu.tw>
2020-11-11 21:18:57 +0800

    Change log_step into config parameters

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -24,9 +24,9 @@ class Trainer(BaseTrainer):
         self.valid_data_loader = valid_data_loader
         self.do_validation = self.valid_data_loader is not None
         self.lr_scheduler = lr_scheduler
-        self.log_step = int(np.sqrt(data_loader.batch_size))
+        self.log_step = config['trainer']['log_step']
 
-        self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
+        self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metrac_ftns], writer=self.writer)
         self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
 
     def _train_epoch(self, epoch):
