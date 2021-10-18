5900001fd2fd8d9e847382b46096dc6730de0ea3
Wu Bo-Run's PC Ubuntu <r08942073@ntu.edu.tw>
2021-10-18 14:30:13 +0800

    Update logger settings

--- logger/logger.py
+++ logger/logger.py
@@ -11,12 +11,13 @@ def setup_logging(save_dir, log_config='logger/logger_config.json', default_leve
     log_config = Path(log_config)
     if log_config.is_file():
         config = read_json(log_config)
-        # modify logging paths based on run config
+
+        # Modify logging paths based on run config
         for _, handler in config['handlers'].items():
             if 'filename' in handler:
                 handler['filename'] = str(save_dir / handler['filename'])
 
         logging.config.dictConfig(config)
     else:
-        print("Warning: logging configuration file is not found in {}.".format(log_config))
+        print('Warning: logging configuration file is not found in {}.'.format(log_config))
         logging.basicConfig(level=default_level)
